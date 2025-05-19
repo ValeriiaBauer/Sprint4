d54656d0406627d3ec412663926ddcfadb7b9900
Valeriia Bauer <valeriiakhu@yandex.ru>
2025-05-19 14:11:48 +0300

    add locators OrderPageScooter

--- src/test/java/ru/praktikum/OrderPageScooter.java
+++ src/test/java/ru/praktikum/OrderPageScooter.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
-import java.util.List;
 
 import org.openqa.selenium.*;
 
@@ -15,22 +14,26 @@ public class OrderPageScooter {
     private final WebDriver driver;
     private final WebDriverWait wait;
 
-    // Локаторы
+    // Локаторы для первой страницы заказа
     private static final By ORDER_HEADER = By.xpath(".//div[text()='Для кого самокат']");
-    private static final By ABOUT_ORDER_HEADER = By.xpath(".//div[text()='Про аренду']");
-    private static final By ACCEPT_COOKIE_BUTTON = By.xpath(".//button[text()='да все привыкли']");
+    private static final By ACCEPT_COOKIE_BUTTON = By.id("rcc-confirm-button");
     private static final By NAME_FIELD = By.xpath(".//input[@placeholder='* Имя']");
     private static final By SURNAME_FIELD = By.xpath(".//input[@placeholder='* Фамилия']");
     private static final By ADDRESS_FIELD = By.xpath(".//input[@placeholder='* Адрес: куда привезти заказ']");
     private static final By SUBWAY_FIELD = By.xpath(".//input[@placeholder='* Станция метро']");
     private static final By PHONE_FIELD = By.xpath(".//input[@placeholder='* Телефон: на него позвонит курьер']");
     private static final By NEXT_BUTTON = By.xpath(".//button[text()='Далее']");
+
+    // Локаторы для второй страницы заказа
+    private static final By ABOUT_ORDER_HEADER = By.xpath(".//div[text()='Про аренду']");
     private static final By DATE_FIELD = By.xpath(".//input[@placeholder='* Когда привезти самокат']");
     private static final By RENTAL_PERIOD_FIELD = By.xpath(".//div[@class='Dropdown-placeholder']");
     private static final By COMMENT_FIELD = By.xpath(".//input[@placeholder='Комментарий для курьера']");
     private static final By ORDER_CREATE_BUTTON = By.xpath("//div[contains(@class,'Order_Buttons')]/button[text()='Заказать']");
     private static final By ORDER_CONFIRM_BUTTON = By.xpath(".//button[text()='Да']");
     private static final By CONFIRM_HEADER = By.xpath(".//div[contains(@class, 'Order_ModalHeader')]");
+
+    // Локаторы для цветов самоката
     private static final By BLACK_COLOR_CHECKBOX = By.id("black");
     private static final By GREY_COLOR_CHECKBOX = By.id("grey");
 
@@ -72,7 +75,7 @@ public class OrderPageScooter {
         try {
             clickElement(ACCEPT_COOKIE_BUTTON);
         } catch (TimeoutException e) {
-            // Куки уже приняты или кнопка не появилась
+            System.out.println("Кнопка принятия куков не найдена или уже принята");
         }
     }
 
@@ -90,10 +93,9 @@ public class OrderPageScooter {
 
     private void setSubway(String subway) {
         clickElement(SUBWAY_FIELD);
-        WebElement station = wait.until(ExpectedConditions.elementToBeClickable(
-                By.xpath(".//div[@class='select-search__select']//*[text()='" + subway + "']")
-        ));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", station);
+        By stationLocator = By.xpath(String.format(".//div[@class='select-search__select']//*[text()='%s']", subway));
+        WebElement station = wait.until(ExpectedConditions.elementToBeClickable(stationLocator));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", station);
         station.click();
     }
 
@@ -108,15 +110,33 @@ public class OrderPageScooter {
 
     private void setDeliveryDate(String date) {
         setInputField(DATE_FIELD, date);
-        // Нажимаем Enter для подтверждения даты
         driver.findElement(DATE_FIELD).sendKeys(Keys.ENTER);
     }
 
     private void setRentalPeriod(String period) {
         clickElement(RENTAL_PERIOD_FIELD);
-        clickElement(By.xpath(".//div[@class='Dropdown-option' and text()='" + period + "']"));
+        By periodOption = By.xpath(String.format(".//div[@class='Dropdown-option' and text()='%s']", period));
+        clickElement(periodOption);
     }
 
+    public void setScooterColor(String color) {
+        String normalizedColor = color.toLowerCase().trim();
+        By colorLocator;
+
+        switch(normalizedColor) {
+            case "черный жемчуг":
+                colorLocator = BLACK_COLOR_CHECKBOX;
+                break;
+            case "серая безысходность":
+                colorLocator = GREY_COLOR_CHECKBOX;
+                break;
+            default:
+                throw new IllegalArgumentException("Неподдерживаемый цвет: " + color);
+        }
+
+        WebElement checkbox = wait.until(ExpectedConditions.elementToBeClickable(colorLocator));
+        ((JavascriptExecutor)driver).executeScript("arguments[0].click();", checkbox);
+    }
 
     private void setComment(String comment) {
         setInputField(COMMENT_FIELD, comment);
@@ -140,25 +160,7 @@ public class OrderPageScooter {
 
     private void clickElement(By locator) {
         WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView();", element);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
         element.click();
-    }
-    public void setScooterColor(String color) {
-        String normalizedColor = color.toLowerCase().trim();
-        By colorLocator;
-
-        switch(normalizedColor) {
-            case "черный жемчуг":
-                colorLocator = By.id("black");
-                break;
-            case "серая безысходность":
-                colorLocator = By.id("grey");
-                break;
-            default:
-                throw new IllegalArgumentException("Неподдерживаемый цвет: " + color);
-        }
-
-        WebElement checkbox = wait.until(ExpectedConditions.elementToBeClickable(colorLocator));
-        ((JavascriptExecutor)driver).executeScript("arguments[0].click();", checkbox);
     }
 }
